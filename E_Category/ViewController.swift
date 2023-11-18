a250d8435db807950a91b52e19ec934ff413d8b8
faxriddin0516 <faxaoff@gmail.com>
2023-11-18 16:30:15 +0500

    Initial Commit

---
+++ E_Category/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E_Category
+//
+//  Created by Faxriddin Mo'ydinxonov on 18/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
