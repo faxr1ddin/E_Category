51aa6122e52f9f7117f13042b9263bf1bdc28b4c
faxriddin0516 <faxaoff@gmail.com>
2023-12-25 20:49:43 +0500

    Demo data has been created for application and it is collection.

---
+++ E_Category/Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  E_Category
+//
+//  Created by Faxriddin Mo'ydinxonov on 25/12/23.
+//
+
+import Foundation
---
+++ E_Category/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  E_Category
+//
+//  Created by Faxriddin Mo'ydinxonov on 25/12/23.
+//
+
+import Foundation
